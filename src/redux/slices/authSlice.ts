c40fb5401f978456f03f79aa1cca45d26b0be5a9
“kenny-tech” <kenny4real2001@gmail.com>
2026-05-31 18:46:48 +0100

    Updated auth slice

--- src/redux/slices/authSlice.ts
+++ src/redux/slices/authSlice.ts
@@ -147,7 +147,6 @@ export const logoutUser = createAsyncThunk(
       // Even if API fails, still clear local storage
       await AsyncStorage.removeItem(APP_CONSTANTS.STORAGE_KEYS.AUTH_TOKEN);
       await AsyncStorage.removeItem(APP_CONSTANTS.STORAGE_KEYS.USER_DATA);
-      // await AsyncStorage.removeItem('userEmail');
     }
   }
 );
